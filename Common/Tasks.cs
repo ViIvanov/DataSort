b5be01bbc3a31677830aaf5271be43d715e6dd0e
Viacheslav Ivanov <viacheslav.ivanov@gmail.com>
2021-10-27 16:40:02 +0300

    Fix `Tasks.WhenAll(…)`
    Provide a correct index to the created task.
    Fixes seed for random, used for data generation.

--- Common/Tasks.cs
+++ Common/Tasks.cs
@@ -5,7 +5,8 @@ public static class Tasks
   public static Task WhenAll(int count, Func<int, Task> factory, CancellationToken cancellationToken = default) {
     var array = new Task[count];
     for(var index = 0; index < array.Length; index++) {
-      array[index] = Task.Run(() => factory(index), cancellationToken);
+      var taskIndex = index;
+      array[index] = Task.Run(() => factory(taskIndex), cancellationToken);
     }//for
     return Task.WhenAll(array);
   }
