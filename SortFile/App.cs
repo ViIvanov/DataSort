1796baa6e723a3fc116a16e271497b01b4eb7d89
Viacheslav Ivanov <viacheslav.ivanov@kofax.com>
2022-09-01 15:09:02 +0200

    Return code for starting memory monitor in SortFile
    Allow to start memory monitor in case when debugger is not attached.

--- SortFile/App.cs
+++ SortFile/App.cs
@@ -38,9 +38,11 @@ internal static class App
         Directory.CreateDirectory(options.File.WorkingDirectory);
       }//if
 
-      //Console.WriteLine($"Sort file \"{Path.GetFileName(options.FilePath)}\" in directory \"{options.File.WorkingDirectory}\".");
-      //Console.WriteLine($"Start memory monitor for process #{Environment.ProcessId} and press <Enter>");
-      //Console.ReadLine();
+      Console.WriteLine($"Sort file \"{Path.GetFileName(options.FilePath)}\" in directory \"{options.File.WorkingDirectory}\".");
+      if(!Debugger.IsAttached) {
+        Console.Write($"Start memory monitor for process #{Environment.ProcessId} and press <Enter>");
+        Console.ReadLine();
+      }//if
 
       var stopwatch = Stopwatch.StartNew();
 
