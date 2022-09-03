d1cd241eb68c7682151e7a28391a176a5b09d099
Viacheslav Ivanov <viacheslav.ivanov@kofax.com>
2022-09-03 03:34:14 +0200

    Upgrade to .NET 7.0

--- GenerateFile.cmd
+++ GenerateFile.cmd
@@ -1,8 +1,8 @@
-@Rem @Cd "GenerateFile\bin\Debug\net6.0"
+@Rem @Cd "GenerateFile\bin\Debug\net7.0"
 @Rem @GenerateFile --FilePath ".\..\..\..\..\Results\DataSort-100M-Debug.txt" --RequiredLengthGiB 0.1
 @Rem @Cd ".\..\..\..\..\"
 
-@Cd "GenerateFile\bin\Release\net6.0"
+@Cd "GenerateFile\bin\Release\net7.0"
 @GenerateFile --FilePath ".\..\..\..\..\Results\DataSort-1G.txt" --RequiredLengthGiB 1
 @GenerateFile --FilePath ".\..\..\..\..\Results\DataSort-10G.txt" --RequiredLengthGiB 10
 @Cd ".\..\..\..\..\"
--- SortFile.cmd
+++ SortFile.cmd
@@ -1,3 +1,3 @@
-@Cd "SortFile\bin\Release\net6.0"
+@Cd "SortFile\bin\Release\net7.0"
 @SortFile --FilePath ".\..\..\..\..\Results\DataSort-10G.txt"  --MaxReadLines 50000
 @Cd ".\..\..\..\..\"
